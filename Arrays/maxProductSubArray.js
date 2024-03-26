ae8113b5411180d26ab715f1aec0cb78f23bc2e0
mohannagabhiru <chiranjeevichowdary7@gmail.com>
2024-03-26 16:17:14 +0530

    Updated problems

--- Arrays/maxProductSubArray.js
+++ Arrays/maxProductSubArray.js
@@ -1,15 +1,23 @@
-let arr = [2, 3, -2, 4]
+let arr = [2,3,-2,4]
 
 function findMaxSub(arr, n){
     let maxProduct = arr[0];
+    let startIndex ;
+    let endIndex ;
     for(let i =0; i < n; i++){
         let accu = 1;
        for( j = i; j < n; j++){
         accu *= arr[j]
-        maxProduct = Math.max(accu, maxProduct)
+        if(accu > maxProduct){
+            startIndex = i;
+            endIndex = j;
+        }
+        
+        // maxProduct = Math.max(accu, maxProduct)
        } 
     }
-    return maxProduct
+    console.log(startIndex, endIndex)
+    return {maxProduct, arr : arr.slice(startIndex, endIndex)}
 }
 
 console.log(findMaxSub(arr, arr.length))
--- Arrays/prodArrExpSelf.js
+++ Arrays/prodArrExpSelf.js
@@ -1,10 +1,16 @@
-let input = [1,2,3,4];
+let input =[-1,1,0,-3,3];
 
 function product(){
     let output = [];
     for (let i = 0; i < input.length; i++){
         let product = 1;
-        for(let j = i + 1; j < input.length; j++){
+        if(i > 0){
+            for (let k = 0; k < i; k++){
+                product *= nums[k]
+            }
+        }
+        for(let j = i + 1; j < input.length; j++){ 
+            console.log("curr", j)
             product *= input[j];
         }
         output[i] = product;
