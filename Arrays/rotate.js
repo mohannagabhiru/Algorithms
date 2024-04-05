8092d7df5d1d13ed6da9d17b6a70569141e51261
mohannagabhiru <chiranjeevichowdary7@gmail.com>
2024-04-05 23:58:12 +0530

    Rotate array added

---
+++ Arrays/rotate.js
@@ -0,0 +1,13 @@
+function rotate(nums, k) {
+    const n = nums.length;
+    k %= n;
+    
+    const rotated = [];
+    for (let i = 0; i < n; i++) {
+        rotated[(i + k) % n] = nums[i];
+    }
+    
+    for (let i = 0; i < n; i++) {
+        nums[i] = rotated[i];
+    }
+}
